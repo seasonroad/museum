bc4fc6bc484bc11491e2b9e69c8ce4e2bf8748e0
Haibo Yang <B40869@freescale.com>
2014-01-23 18:29:08 +0800

    tmp change v3

--- museum_main/views.py
+++ museum_main/views.py
@@ -34,6 +34,28 @@ def add_village():
     return redirect(url_for('show_villages'))
 
 
+@app.route('/jxgd/what')
+def what():
+    a={"files": [
+  {
+    "name": "picture1.jpg",
+    "size": 902604,
+    "url": "http:\/\/example.org\/files\/picture1.jpg",
+    "thumbnailUrl": "http:\/\/example.org\/files\/thumbnail\/picture1.jpg",
+    "deleteUrl": "http:\/\/example.org\/files\/picture1.jpg",
+    "deleteType": "DELETE"
+  },
+  {
+    "name": "picture2.jpg",
+    "size": 841946,
+    "url": "http:\/\/example.org\/files\/picture2.jpg",
+    "thumbnailUrl": "http:\/\/example.org\/files\/thumbnail\/picture2.jpg",
+    "deleteUrl": "http:\/\/example.org\/files\/picture2.jpg",
+    "deleteType": "DELETE"
+  }
+]}
+    return a
+
 #----------------------------------------------------#
 #     login / logout management                      #
 #----------------------------------------------------#
